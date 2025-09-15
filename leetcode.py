0c95e2b794cc08d87784c8eecd5c647f893108e3
shyal <shyal@shyal.com>
2025-09-15 08:19:15 +0000

    867. Transpose Matrix

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,145 @@
 from typing import List
 
+
 """
+https://leetcode.com/problems/base-7/description/
+
+504. Base 7
+Easy
+Given an integer num, return a string of its base 7 representation.
+
+Example 1:
+
+Input: num = 100
+Output: "202"
+Example 2:
+
+Input: num = -7
+Output: "-10"
+
+Constraints:
+
+-107 <= num <= 107
+"""
+
+
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        res = ""
+        sign = 1 if num >= 0 else -1
+        num *= sign
+        while num > 0:
+            val, r = divmod(num, 7)
+            res += str(r)
+            num = val
+        res = ("-" if sign == -1 else "") + res[::-1]
+        return res
+
+
+sol = Solution()
+assert sol.convertToBase7(100) == "202"
+assert sol.convertToBase7(-7) == "-10"
+assert sol.convertToBase7(0) == "0"
+assert sol.convertToBase7(1) == "1"
+assert sol.convertToBase7(6) == "6"
+assert sol.convertToBase7(7) == "10"
+assert sol.convertToBase7(8) == "11"
+assert sol.convertToBase7(48) == "66"
+assert sol.convertToBase7(49) == "100"
+assert sol.convertToBase7(50) == "101"
+assert sol.convertToBase7(-1) == "-1"
+assert sol.convertToBase7(-6) == "-6"
+assert sol.convertToBase7(-8) == "-11"
+assert sol.convertToBase7(343) == "1000"
+assert sol.convertToBase7(-343) == "-1000"
+assert sol.convertToBase7(1000000) == "11333311"
+assert sol.convertToBase7(-1000000) == "-11333311"
+
+
+"""
+https://leetcode.com/problems/transpose-matrix/description/
+
+867. Transpose Matrix
+Easy
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+Example 1:
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+Example 2:
+
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+"""
+
+"""
+1 2 3
+4 5 6
+7 8 9
+
+
+"""
+
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        return [list(x) for x in zip(*matrix)]
+
+
+sol = Solution()
+assert sol.transpose(matrix=[[1, 2, 3], [4, 5, 6], [7, 8, 9]]) == [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+]
+
+assert sol.transpose(matrix=[[1, 2, 3], [4, 5, 6]]) == [[1, 4], [2, 5], [3, 6]]
+assert sol.transpose(matrix=[[1]]) == [[1]]
+assert sol.transpose(matrix=[[1, 2], [3, 4]]) == [[1, 3], [2, 4]]
+assert sol.transpose(matrix=[[5]]) == [[5]]
+assert sol.transpose(matrix=[[1, 2, 3, 4]]) == [[1], [2], [3], [4]]
+assert sol.transpose(matrix=[[1], [2], [3], [4]]) == [[1, 2, 3, 4]]
+assert sol.transpose(matrix=[[0, 0, 0], [0, 0, 0], [0, 0, 0]]) == [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+assert sol.transpose(matrix=[[1, 2], [3, 4], [5, 6]]) == [[1, 3, 5], [2, 4, 6]]
+assert sol.transpose(matrix=[[1, 3, 5], [2, 4, 6]]) == [[1, 2], [3, 4], [5, 6]]
+assert sol.transpose(matrix=[[-1, -2], [-3, -4]]) == [[-1, -3], [-2, -4]]
+assert sol.transpose(matrix=[[10, 20, 30], [40, 50, 60]]) == [
+    [10, 40],
+    [20, 50],
+    [30, 60],
+]
+assert sol.transpose(matrix=[[7, 8, 9], [1, 2, 3], [4, 5, 6]]) == [
+    [7, 1, 4],
+    [8, 2, 5],
+    [9, 3, 6],
+]
+assert sol.transpose(matrix=[[2]]) == [[2]]
+assert sol.transpose(matrix=[[1, 2, 3]]) == [[1], [2], [3]]
+assert sol.transpose(matrix=[[1], [2], [3]]) == [[1, 2, 3]]
+assert sol.transpose(matrix=[[0]]) == [[0]]
+
+
+"""
+https://leetcode.com/problems/valid-boomerang/
+
 1037 - Valid Boomerang
 
 Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
@@ -72,22 +211,16 @@ assert sol.isBoomerang([[2, 2], [3, 3], [1, 1]]) == False
 
 
 """
+https://leetcode.com/problems/water-bottles/
+
 1518. Water Bottles
-Easy
-Topics
-premium lock icon
-Companies
-Hint
 There are numBottles water bottles that are initially full of water. You can exchange numExchange empty water bottles from the market with one full water bottle.
 
 The operation of drinking a full water bottle turns it into an empty bottle.
 
 Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
 
- 
-
 Example 1:
-
 
 Input: numBottles = 9, numExchange = 3
 Output: 13
@@ -137,64 +270,3 @@ assert sol.numWaterBottles(3, 2) == 5
 assert sol.numWaterBottles(50, 5) == 62
 assert sol.numWaterBottles(20, 6) == 23
 assert sol.numWaterBottles(8, 4) == 10
-
-
-"""
-504. Base 7
-Easy
-Topics
-premium lock icon
-Companies
-Given an integer num, return a string of its base 7 representation.
-
- 
-
-Example 1:
-
-Input: num = 100
-Output: "202"
-Example 2:
-
-Input: num = -7
-Output: "-10"
- 
-
-Constraints:
-
--107 <= num <= 107
-"""
-
-
-class Solution:
-    def convertToBase7(self, num: int) -> str:
-        if num == 0:
-            return "0"
-        res = ""
-        sign = 1 if num >= 0 else -1
-        num *= sign
-        while num > 0:
-            val, r = divmod(num, 7)
-            res += str(r)
-            num = val
-        res = ("-" if sign == -1 else "") + res[::-1]
-        return res
-
-
-sol = Solution()
-assert sol.convertToBase7(100) == "202"
-assert sol.convertToBase7(-7) == "-10"
-assert sol.convertToBase7(0) == "0"
-assert sol.convertToBase7(1) == "1"
-assert sol.convertToBase7(6) == "6"
-assert sol.convertToBase7(7) == "10"
-assert sol.convertToBase7(8) == "11"
-assert sol.convertToBase7(48) == "66"
-assert sol.convertToBase7(49) == "100"
-assert sol.convertToBase7(50) == "101"
-assert sol.convertToBase7(-1) == "-1"
-assert sol.convertToBase7(-6) == "-6"
-assert sol.convertToBase7(-8) == "-11"
-assert sol.convertToBase7(343) == "1000"
-assert sol.convertToBase7(-343) == "-1000"
-assert sol.convertToBase7(1000000) == "11333311"
-assert sol.convertToBase7(-1000000) == "-11333311"
