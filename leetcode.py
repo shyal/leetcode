7e8523036aaf424c206271505c70e6a3a0809ac0
shyal <shyal@shyal.com>
2025-09-17 05:02:54 +0000

    stub

--- leetcode.py
+++ leetcode.py
@@ -179,6 +179,53 @@ assert sol.twoSum(numbers=[999, 1000], target=1999) == [1, 2]
 assert sol.twoSum(numbers=[-1, 0, 0, 1], target=0) == [1, 4]  # -1 + 1 = 0
 
 """
+https://leetcode.com/problems/increasing-triplet-subsequence/description
+
+334. Increasing Triplet Subsequence
+Medium
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: true
+Explanation: Any triplet where i < j < k is valid.
+Example 2:
+
+Input: nums = [5,4,3,2,1]
+Output: false
+Explanation: No triplet exists.
+Example 3:
+
+Input: nums = [2,1,5,0,4,6]
+Output: true
+Explanation: One of the valid triplet is (3, 4, 5), because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 105
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+"""
+
+
+class Solution:  # stub
+    def increasingTriplet(self, nums: List[int], N=3) -> bool:
+        # todo
+        pass
+
+
+if False:
+    sol = Solution()
+    assert sol.increasingTriplet([1, 2, 3, 4, 5]) == True
+    assert sol.increasingTriplet([5, 4, 3, 2, 1]) == False
+    assert sol.increasingTriplet([2, 1, 5, 0, 4, 6]) == True
+
+
+"""
 345. Reverse Vowels of a String
 Easy
 Given a string s, reverse only all the vowels in the string and return it.
