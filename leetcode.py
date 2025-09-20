913a768d1e3ba5439c122268fa26cf86dcb55708
shyal <shyal@shyal.com>
2025-09-20 05:22:00 +0000

    136. Single Number

--- leetcode.py
+++ leetcode.py
@@ -84,6 +84,56 @@ assert sol.maxDepth(tree9) == 4
 tree10 = build_tree([1, None, 2, None, 3])
 assert sol.maxDepth(tree10) == 3
 """
+URL: https://leetcode.com/problems/single-number/description/?envType=study-plan-v2&envId=leetcode-75
+
+136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+
+Constraints:
+
+        1 <= nums.length <= 3 * 104
+        -3 * 104 <= nums[i] <= 3 * 104
+        Each element in the array appears twice except for one element which appears only once.
+"""
+
+from typing import List
+from functools import reduce
+from operator import xor
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(xor, nums)
+
+
+sol = Solution()
+assert sol.singleNumber([2, 2, 1]) == 1
+assert sol.singleNumber([4, 1, 2, 1, 2]) == 4
+assert sol.singleNumber([1]) == 1
+"""
 151. Reverse Words in a String
 Medium
 Given an input string s, reverse the order of the words.
@@ -2896,4 +2946,5 @@ assert sol.removeStars("abc***") == ""
 assert sol.removeStars("abcd***") == "a"
 assert sol.removeStars("aa*bb*cc*") == "abc"
 assert sol.removeStars("ab*cdef**") == "acd"
+
 
