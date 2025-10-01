efd72fd36b9d39b290be5b809f94e55a431562d1
shyal <shyal@shyal.com>
2025-10-01 00:35:42 +0000

    509. Fibonacci Number
    solve time: 2m 35s
    
    remember condition n <= 1

--- leetcode_hard.py
+++ leetcode_hard.py
@@ -188,3 +188,58 @@ assert sol.divisorGame(2) == True
 assert sol.divisorGame(3) == False
 assert sol.divisorGame(4) == True
 assert sol.divisorGame(8) == True
+"""
+URL: https://leetcode.com/problems/fibonacci-number/description/
+
+509. Fibonacci Number
+
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+
+Constraints:
+
+    0 <= n <= 30
+"""
+
+from functools import cache
+
+
+class Solution:
+    @cache
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
+
+
+sol = Solution()
+
+assert sol.fib(2) == 1
+assert sol.fib(3) == 2
+assert sol.fib(4) == 3
+
+
