048ab56b4c394d484f4c792a1089361413e8504b
shyal <shyal@shyal.com>
2025-10-03 07:24:30 +0000

    543. Diameter of Binary Tree
    solve time: 20m 0s
    still learning - reschedule

---
+++ solved/p543_Diameter_of_Binary_Tree_2025_10_03T07_24_30_329956_00_00Z.py
@@ -0,0 +1,95 @@
+"""
+URL: https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+543. Diameter of Binary Tree
+
+Given the root of a binary tree, return the length of the diameter of the tree.
+
+The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+
+The length of a path between two nodes is represented by the number of edges between them.
+
+
+Example 1:
+
+Input: root = [1,2,3,4,5]
+
+Output: 3
+
+Explanation: 3 is the length of the path [4,2,1,3] or [5,2,1,3].
+
+Example 2:
+
+Input: root = [1,2]
+
+Output: 1
+
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 104].
+
+    -100 <= Node.val <= 100
+
+---
+
+I think the diameter is essentially the max of the depth on the left subtree
+and and the right subtree. This definition works recursively.
+
+"""
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, res):
+            if not node:
+                return 0
+            left = dfs(node.left, res)
+            right = dfs(node.right, res)
+            res[0] = max(res[0], left + right)
+            return max(left, right) + 1
+
+        res = [0]
+        dfs(root, res)
+        return res[0]
+
+
+sol = Solution()
+assert sol.diameterOfBinaryTree(build_tree([1, 2, 3, 4, 5])) == 3
+assert sol.diameterOfBinaryTree(build_tree([1, 2])) == 1
+assert (
+    sol.diameterOfBinaryTree(
+        build_tree(
+            [
+                7,
+                6,
+                9,
+                4,
+                None,
+                8,
+                10,
+                1,
+                5,
+                None,
+                None,
+                None,
+                None,
+                None,
+                2,
+                None,
+                None,
+                None,
+                3,
+            ]
+        )
+    )
+    == 7
+)
+assert sol.diameterOfBinaryTree(build_tree([1])) == 0
+assert sol.diameterOfBinaryTree(build_tree([1, 2, 3])) == 2
+assert sol.diameterOfBinaryTree(build_tree([1, 2, 3, 4])) == 3
+tree = build_tree([1, 2, None, 3, 4, 5, 6, 7, 8])
+res = sol.diameterOfBinaryTree(tree)
+draw_tree(tree)
+assert res == 4
+assert sol.diameterOfBinaryTree(build_tree([1, None, 2])) == 1
