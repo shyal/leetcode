c52cbd3e525b1940ca5deaaee17a731e83009c87
shyal <shyal@shyal.com>
2025-10-10 21:43:29 +0000

    153. Find Minimum in Rotated Sorted Array
    solve time: 20m 39s

---
+++ solved/p153_Find_Minimum_in_Rotated_Sorted_Array_2025_10_10T21_43_29_778480_00_00Z.py
@@ -0,0 +1,85 @@
+"""
+URL: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+153. Find Minimum in Rotated Sorted Array
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+    [4,5,6,7,0,1,2] if it was rotated 4 times.
+    [0,1,2,4,5,6,7] if it was rotated 7 times.
+
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 5000
+    -5000 <= nums[i] <= 5000
+    All the integers of nums are unique.
+    nums is sorted and rotated between 1 and n times.
+"""
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        _min = float("inf")
+        while left <= right:
+            mid = (left + right) // 2
+            left_is_sorted = nums[left] < nums[mid]
+            if right - left == 1:
+                return min(
+                    _min, nums[left] if nums[left] < nums[right] else nums[right]
+                )
+            if left_is_sorted:
+                _min = min(_min, nums[left])
+                left = mid + 1
+            else:
+                _min = min(_min, nums[mid])
+                right = mid - 1
+        return _min
+
+
+sol = Solution()
+
+# print(sol.findMin([2, 3, 4, 5, 1]))  # 1
+
+assert sol.findMin([3, 4, 5, 1, 2]) == 1
+assert sol.findMin([4, 5, 6, 7, 0, 1, 2]) == 0
+assert sol.findMin([11, 13, 15, 17]) == 11
+assert sol.findMin([1]) == 1
+assert sol.findMin([2, 1]) == 1
+assert sol.findMin([1, 2]) == 1
+assert sol.findMin([5, 6, 7, 8, 9, 1, 2, 3, 4]) == 1
+assert sol.findMin([-5, -4, -3, -2, -1]) == -5
+assert sol.findMin([-2, -1, -5, -4, -3]) == -5
+assert sol.findMin([3, 4, 5, 6, 7, 8, 9, 10, 1, 2]) == 1
+assert sol.findMin([10, 1, 2, 3, 4, 5, 6, 7, 8, 9]) == 1
+assert sol.findMin([-1, 0, 1, 2, 3]) == -1
+assert sol.findMin([2, 3, -1, 0, 1]) == -1
+assert sol.findMin([5000, -5000]) == -5000
+assert sol.findMin([-5000, 5000]) == -5000
