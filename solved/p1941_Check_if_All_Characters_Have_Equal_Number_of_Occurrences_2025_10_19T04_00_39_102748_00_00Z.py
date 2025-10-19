f9daaeafd943026a52fc93ca2cddd47ccf712995
shyal <shyal@shyal.com>
2025-10-19 04:00:39 +0000

    1941. Check if All Characters Have Equal Number of Occurrences
    solve time: 1m 7s

---
+++ solved/p1941_Check_if_All_Characters_Have_Equal_Number_of_Occurrences_2025_10_19T04_00_39_102748_00_00Z.py
@@ -0,0 +1,39 @@
+"""
+URL: https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/description/?envType=problem-list-v2&envId=vn57k9wr
+
+1941. Check if All Characters Have Equal Number of Occurrences
+
+Given a string s, return true if s is a good string, or false otherwise.
+
+A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+
+
+Example 1:
+
+Input: s = "abacbc"
+Output: true
+Explanation: The characters that appear in s are 'a', 'b', and 'c'. All characters occur 2 times in s.
+
+Example 2:
+
+Input: s = "aaabb"
+Output: false
+Explanation: The characters that appear in s are 'a' and 'b'.
+'a' occurs 3 times while 'b' occurs 2 times, which is not the same number of times.
+
+
+Constraints:
+
+        1 <= s.length <= 1000
+        s consists of lowercase English letters.
+"""
+
+
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        return len(set(Counter(s).values())) == 1
+
+
+sol = Solution()
+res = sol.areOccurrencesEqual(s="abacbc")
+assert res == True
