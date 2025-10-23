1e728ae3e9ef34ca6a6cee8c85897ae07a7541f1
shyal <shyal@shyal.com>
2025-10-23 02:48:57 +0000

    15. 3Sum
    solve time: 11m 16s
    still learning - reschedule

---
+++ solved/p15_3Sum_2025_10_23T02_48_57_349295_00_00Z.py
@@ -0,0 +1,68 @@
+"""
+URL: https://leetcode.com/problems/3sum/description/
+
+15. 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+
+    3 <= nums.length <= 3000
+    -105 <= nums[i] <= 105
+
+---
+
+Botched this again... i forgot to sort, so got confused why there were some repeats.
+
+Mildly annoying.
+
+"""
+
+
+class Solution:
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+        seen = set()
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i - 1] == nums[i]:
+                continue
+            target = -nums[i]
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[j]
+                if complement in seen:
+                    res.add(tuple(sorted((nums[i], nums[j], complement))))
+                seen.add(nums[j])
+        return [list(x) for x in sorted(res)]
+
+
+sol = Solution()
+res = sol.threeSum(nums=[-1, 0, 1, 2, -1, -4])
