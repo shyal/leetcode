b100748b4f6fcd1165162f2b63eefe3bd9b74090
shyal <shyal@shyal.com>
2025-11-04 22:05:35 +0000

    1319. Number of Operations to Make Network Connected
    solve time: 6m 34s

---
+++ solved/p1319_Number_of_Operations_to_Make_Network_Connected_2025_11_04T22_05_35_698478_00_00Z.py
@@ -0,0 +1,134 @@
+"""
+URL: https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/?envType=problem-list-v2&envId=vn57k9wr
+
+1319. Number of Operations to Make Network Connected
+
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+Example 1:
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+
+Example 2:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+
+Constraints:
+
+    1 <= n <= 10^5
+    1 <= connections.length <= min(n * (n - 1) / 2, 10^5)
+    connections[i].length == 2
+    0 <= ai, bi < n
+    ai != bi
+    There are no repeated connections.
+    No two computers are connected by more than one cable.
+"""
+
+
+class Solution:
+
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        parent = list(range(n))
+        self.networks = n
+        self.redundant = 0
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            rootx = find(x)
+            rooty = find(y)
+            if rootx != rooty:
+                parent[rootx] = rooty
+                self.networks -= 1
+            else:
+                self.redundant += 1
+
+        for x, y in connections:
+            union(x, y)
+
+        if self.redundant >= self.networks - 1:
+            return self.networks - 1
+        return -1
+
+
+sol = Solution()
+
+edges = [[0, 1], [0, 2], [1, 2]]
+draw_graphviz(edges, n=4)
+assert sol.makeConnected(4, edges) == 1
+edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]
+draw_graphviz(edges, n=6)
+assert sol.makeConnected(6, edges) == 2
+edges = []
+draw_graphviz(edges, n=1)
+assert sol.makeConnected(1, edges) == 0
+edges = [[0, 1]]
+draw_graphviz(edges, n=2)
+assert sol.makeConnected(2, edges) == 0
+edges = [[0, 1], [0, 2]]
+draw_graphviz(edges, n=3)
+assert sol.makeConnected(3, edges) == 0
+edges = [[0, 1], [1, 2], [0, 2]]
+draw_graphviz(edges, n=3)
+assert sol.makeConnected(3, edges) == 0
+edges = [[0, 1], [0, 2], [0, 3], [0, 4]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == 0
+edges = [[0, 1], [0, 2], [1, 2], [3, 4]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == 1
+edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == 1
+edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [4, 5], [4, 6]]
+draw_graphviz(edges, n=7)
+assert sol.makeConnected(7, edges) == 1
+edges = [[1, 2], [1, 3], [1, 4], [2, 3]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == 1
+edges = [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [4, 5]]
+draw_graphviz(edges, n=6)
+assert sol.makeConnected(6, edges) == 1
+edges = [[0, 1], [0, 3], [0, 4], [1, 3]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == 1
+edges = []
+draw_graphviz(edges, n=2)
+assert sol.makeConnected(2, edges) == -1
+edges = [[0, 1]]
+draw_graphviz(edges, n=3)
+assert sol.makeConnected(3, edges) == -1
+edges = [[0, 1], [2, 3]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == -1
+edges = []
+draw_graphviz(edges, n=4)
+assert sol.makeConnected(4, edges) == -1
+edges = [[0, 2]]
+draw_graphviz(edges, n=3)
+assert sol.makeConnected(3, edges) == -1
+edges = [[0, 2], [0, 3]]
+draw_graphviz(edges, n=4)
+assert sol.makeConnected(4, edges) == -1
+edges = [[0, 1], [0, 2], [0, 3], [1, 2]]
+draw_graphviz(edges, n=6)
+assert sol.makeConnected(6, edges) == -1
+edges = [[2, 3], [3, 4]]
+draw_graphviz(edges, n=5)
+assert sol.makeConnected(5, edges) == -1
