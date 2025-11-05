0af8d172cf4782971e0560c87ff38c327fcc6a6a
shyal <shyal@shyal.com>
2025-11-05 08:49:22 +0000

    2161. Partition Array According to Given Pivot
    solve time: 6m 5s

---
+++ solved/p2161_Partition_Array_According_to_Given_Pivot_2025_11_05T08_49_22_013487_00_00Z.py
@@ -0,0 +1,64 @@
+"""
+URL: https://leetcode.com/problems/partition-array-according-to-given-pivot/description/?envType=problem-list-v2&envId=vn57k9wr
+
+2161. Partition Array According to Given Pivot
+
+You are given a 0-indexed integer array nums and an integer pivot. Rearrange nums such that the following conditions are satisfied:
+
+- Every element less than pivot appears before every element greater than pivot.
+- Every element equal to pivot appears in between the elements less than and greater than pivot.
+- The relative order of the elements less than pivot and the elements greater than pivot is maintained.
+  - More formally, consider every p_i, p_j where p_i is the new position of the i-th element and p_j is the new position of the j-th element. If i < j and both elements are smaller (or larger) than pivot, then p_i < p_j.
+
+Return nums after the rearrangement.
+
+
+Example 1:
+
+Input: nums = [9,12,5,10,14,3,10], pivot = 10
+Output: [9,5,3,10,10,12,14]
+Explanation:
+The elements 9, 5, and 3 are less than the pivot so they are on the left side of the array.
+The elements 12 and 14 are greater than the pivot so they are on the right side of the array.
+The relative ordering of the elements less than and greater than pivot is also maintained. [9, 5, 3] and [12, 14] are the respective orderings.
+
+Example 2:
+
+Input: nums = [-3,4,3,2], pivot = 2
+Output: [-3,2,4,3]
+Explanation:
+The element -3 is less than the pivot so it is on the left side of the array.
+The elements 4 and 3 are greater than the pivot so they are on the right side of the array.
+The relative ordering of the elements less than and greater than pivot is also maintained. [-3] and [4, 3] are the respective orderings.
+
+
+Constraints:
+
+- 1 <= nums.length <= 10^5
+- -10^6 <= nums[i] <= 10^6
+- pivot equals to an element of nums.
+"""
+
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        nums.sort(
+            key=lambda x: -maxsize if x < pivot else (x if x == pivot else maxsize)
+        )
+        return nums
+
+
+sol = Solution()
+
+# print(sol.pivotArray([9, 12, 5, 10, 14, 3, 10], 10))  # [9,5,3,10,10,12,14]
+
+assert sol.pivotArray([9, 12, 5, 10, 14, 3, 10], 10) == [9, 5, 3, 10, 10, 12, 14]
+assert sol.pivotArray([-3, 4, 3, 2], 2) == [-3, 2, 4, 3]
+assert sol.pivotArray([5], 5) == [5]
+assert sol.pivotArray([1, 1, 1], 1) == [1, 1, 1]
+assert sol.pivotArray([10, 12, 14], 10) == [10, 12, 14]
+assert sol.pivotArray([9, 5, 3, 10], 10) == [9, 5, 3, 10]
+assert sol.pivotArray([-5, -1, -3, 0, 2, 4], 0) == [-5, -1, -3, 0, 2, 4]
+assert sol.pivotArray([2, 1, 2, 3, 2], 2) == [1, 2, 2, 2, 3]
+assert sol.pivotArray([-1, -4, -2, -3, 0], -2) == [-4, -3, -2, -1, 0]
+assert sol.pivotArray([4, 1, 5, 2, 6, 3], 3) == [1, 2, 3, 4, 5, 6]
