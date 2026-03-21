b3fd16ec3d38bbe31e4c4777e07417e0a2fc7525
shyal <shyal@shyal.com>
2026-03-21 00:04:47 +0000

    136. Single Number
    solve time: 2m 29s

---
+++ solved/p136_Single_Number_2026_03_21T00_04_47_690613_00_00Z.py
@@ -0,0 +1,54 @@
+"""
+URL: https://leetcode.com/problems/single-number/description/?envType=problem-list-v2&envId=vn57k9wr
+
+136. Single Number
+
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+Output: 1
+
+Constraints:
+
+    1 <= nums.length <= 3 * 10^4
+    -3 * 10^4 <= nums[i] <= 3 * 10^4
+    Each element in the array appears twice except for one element which appears only once.
+"""
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(xor, nums)
+
+
+sol = Solution()
+
+print(sol.singleNumber([2,2,1]))  # 1
+
+assert sol.singleNumber([2,2,1]) == 1
+assert sol.singleNumber([4,1,2,1,2]) == 4
+assert sol.singleNumber([1]) == 1
+assert sol.singleNumber([-1]) == -1
+assert sol.singleNumber([1,1,-1]) == -1
+assert sol.singleNumber([-2,-2,-1]) == -1
+assert sol.singleNumber([0,0,1]) == 1
+assert sol.singleNumber([0]) == 0
+assert sol.singleNumber([30000,30000,-30000]) == -30000
+assert sol.singleNumber([-30000,-30000,30000,30000,0]) == 0
+assert sol.singleNumber([1,2,3,4,5,1,2,3,4]) == 5
+assert sol.singleNumber([7,1,1,2,2]) == 7
+assert sol.singleNumber([1,1,2,2,3]) == 3
