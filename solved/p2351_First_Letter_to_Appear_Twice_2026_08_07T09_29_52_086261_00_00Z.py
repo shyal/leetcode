8515acd7b517cd5c9b76b635f458cd1527776482
shyal <shyal@shyal.com>
2026-08-07 09:29:52 +0000

    2351. First Letter to Appear Twice
    solve time: 1m 44s

---
+++ solved/p2351_First_Letter_to_Appear_Twice_2026_08_07T09_29_52_086261_00_00Z.py
@@ -0,0 +1,67 @@
+"""
+URL: https://leetcode.com/problems/first-letter-to-appear-twice/description/?envType=problem-list-v2&envId=vn57k9wr
+
+2351. First Letter to Appear Twice
+
+Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+
+Note:
+
+    - A letter a appears twice before another letter b if the second occurrence of a is before the
+      second occurrence of b.
+    - s will contain at least one letter that appears twice.
+
+
+Example 1:
+
+Input: s = "abccbaacz"
+Output: "c"
+Explanation:
+The letter 'a' appears on the indexes 0, 5 and 6.
+The letter 'b' appears on the indexes 1 and 4.
+The letter 'c' appears on the indexes 2, 3 and 7.
+The letter 'z' appears on the index 8.
+The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
+
+Example 2:
+
+Input: s = "abcdd"
+Output: "d"
+Explanation: The only letter that appears twice is 'd' so we return 'd'.
+
+
+Constraints:
+
+    2 <= s.length <= 100
+    s consists of lowercase English letters.
+    s has at least one repeated letter.
+"""
+
+
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        S = set([])
+        for c in s:
+            if c in S:
+                return c
+            S.add(c)
+
+
+sol = Solution()
+
+# print(sol.repeatedCharacter("abccbaacz"))  # "c"
+
+assert sol.repeatedCharacter("abccbaacz") == "c"
+assert sol.repeatedCharacter("abcdd") == "d"
+assert sol.repeatedCharacter("aa") == "a"
+assert sol.repeatedCharacter("zz") == "z"
+assert sol.repeatedCharacter("aaa") == "a"
+assert sol.repeatedCharacter("abba") == "b"
+assert sol.repeatedCharacter("abab") == "a"
+assert sol.repeatedCharacter("aabb") == "a"
+assert sol.repeatedCharacter("abca") == "a"
+assert sol.repeatedCharacter("xyzzyx") == "z"
+assert sol.repeatedCharacter("abcdeedcba") == "e"
+assert sol.repeatedCharacter("abcdefghijklmnopqrstuvwxyza") == "a"
+assert sol.repeatedCharacter("abcdefghijklmnopqrstuvwxyzz") == "z"
+assert sol.repeatedCharacter("ba" * 50) == "b"
