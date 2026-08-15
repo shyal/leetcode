abfa9865fedeba6ef60f891b036d532735985423
shyal <shyal@shyal.com>
2026-08-15 00:54:14 +0000

    374. Guess Number Higher or Lower
    solve time: 6m 16s

---
+++ solved/p374_Guess_Number_Higher_or_Lower_2026_08_15T00_54_14_577254_00_00Z.py
@@ -0,0 +1,117 @@
+"""
+URL: https://leetcode.com/problems/guess-number-higher-or-lower/description/?envType=problem-list-v2&envId=vn57k9wr
+
+374. Guess Number Higher or Lower
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked
+(the number I picked stays the same throughout the game).
+
+Every time you guess wrong, I will tell you whether the number I picked
+is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+    -1: Your guess is higher than the number I picked (i.e. num > pick).
+    1: Your guess is lower than the number I picked (i.e. num < pick).
+    0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+
+
+Constraints:
+
+    1 <= n <= 2^31 - 1
+    1 <= pick <= n
+"""
+
+# Local stand-in for LeetCode's pre-defined API.
+pick = 0
+
+
+def guess(num: int) -> int:
+    if num > pick:
+        return -1
+    if num < pick:
+        return 1
+    return 0
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 0, n
+        while left <= right:
+            mid = (left + right) // 2
+            res = guess(mid)
+            if res == -1:
+                right = mid - 1
+            elif res == 1:
+                left = mid + 1
+            else:
+                return mid
+
+
+
+sol = Solution()
+
+pick = 6
+print(sol.guessNumber(10))  # 6
+
+pick = 6
+assert sol.guessNumber(10) == 6
+
+pick = 1
+assert sol.guessNumber(1) == 1
+
+pick = 1
+assert sol.guessNumber(2) == 1
+
+pick = 2
+assert sol.guessNumber(2) == 2
+
+pick = 1
+assert sol.guessNumber(10) == 1
+
+pick = 10
+assert sol.guessNumber(10) == 10
+
+pick = 5
+assert sol.guessNumber(10) == 5
+
+pick = 7
+assert sol.guessNumber(10) == 7
+
+pick = 1
+assert sol.guessNumber(2**31 - 1) == 1
+
+pick = 2**31 - 1
+assert sol.guessNumber(2**31 - 1) == 2**31 - 1
+
+pick = 2**30
+assert sol.guessNumber(2**31 - 1) == 2**30
+
+pick = 1073741823
+assert sol.guessNumber(2**31 - 1) == 1073741823
+
+pick = 2
+assert sol.guessNumber(3) == 2
+
+for pick in range(1, 101):
+    assert sol.guessNumber(100) == pick
