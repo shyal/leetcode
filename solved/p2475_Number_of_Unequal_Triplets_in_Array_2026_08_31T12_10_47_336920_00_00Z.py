ab7ed22058c09d89d2feeb29c60e7cc5db195e12
shyal <shyal@shyal.com>
2026-08-31 20:11:50 +0800

    2475. Number of Unequal Triplets in Array
    solve time: 13m 5s

---
+++ solved/p2475_Number_of_Unequal_Triplets_in_Array_2026_08_31T12_10_47_336920_00_00Z.py
@@ -0,0 +1,78 @@
+"""
+URL: https://leetcode.com/problems/number-of-unequal-triplets-in-array/description/?envType=problem-list-v2&envId=vn57k9wr
+
+2475. Number of Unequal Triplets in Array
+
+You are given a 0-indexed array of positive integers nums. Find the number of triplets (i, j, k) that meet the following conditions:
+
+- 0 <= i < j < k < nums.length
+- nums[i], nums[j], and nums[k] are pairwise distinct.
+  - In other words, nums[i] != nums[j], nums[i] != nums[k], and nums[j] != nums[k].
+
+Return the number of triplets that meet the conditions.
+
+Example 1:
+
+Input: nums = [4,4,2,4,3]
+Output: 3
+Explanation: The following triplets meet the conditions:
+- (0, 2, 4) because 4 != 2 != 3
+- (1, 2, 4) because 4 != 2 != 3
+- (2, 3, 4) because 2 != 4 != 3
+Since there are 3 triplets, we return 3.
+Note that (2, 0, 4) is not a valid triplet because 2 > 0.
+
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: 0
+Explanation: No triplets meet the conditions so we return 0.
+
+Constraints:
+
+    3 <= nums.length <= 100
+    1 <= nums[i] <= 1000
+
+---
+
+Assisted because didn't read the question properly.
+
+did nums[i] != nums[j] != nums[k] instead of the proper check.
+
+Else it would have been a clean solve.
+
+There should be a node for "didn't read the question properly".
+
+"""
+
+
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        seen = set([])
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                for k in range(j + 1, len(nums)):
+                    if nums[i] != nums[j] and nums[j] != nums[k] and nums[i] != nums[k]:
+                        seen.add((i, j, k))
+        return len(seen)
+
+
+sol = Solution()
+
+print(sol.unequalTriplets([4, 4, 2, 4, 3]))  # 3
+
+assert sol.unequalTriplets([4, 4, 2, 4, 3]) == 3
+assert sol.unequalTriplets([1, 1, 1, 1, 1]) == 0
+
+assert sol.unequalTriplets([1, 2, 3]) == 1
+assert sol.unequalTriplets([1, 1, 2]) == 0
+assert sol.unequalTriplets([1, 2, 2, 3]) == 2
+assert sol.unequalTriplets([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) == 120
+assert sol.unequalTriplets([1000] * 100) == 0
+assert sol.unequalTriplets([1, 2, 3] * 33 + [4]) == 39204
+assert sol.unequalTriplets([1, 1, 2, 2, 3, 3]) == 8
+assert sol.unequalTriplets([1, 2, 3, 1, 2, 3, 1, 2, 3]) == 27
+assert sol.unequalTriplets([1, 2, 3, 4, 5, 6, 7, 8, 9, 10] * 10) == 120000
+assert sol.unequalTriplets([1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]) == 97
+assert sol.unequalTriplets([1, 2, 3, 4]) == 4
+assert sol.unequalTriplets([1, 1, 1]) == 0
