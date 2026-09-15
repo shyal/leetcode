fcb033c9de3ff445244a2306aa99429d5449a889
shyal <shyal@shyal.com>
2026-09-15 12:29:16 +0000

    drill: Order By Other Array
    solve time: 3m 31s

--- solved/d_Order_By_Other_Array_2026_09_15T12_29_16_228887_00_00Z.py
+++ solved/d_Order_By_Other_Array_2026_09_15T12_29_16_228887_00_00Z.py
@@ -34,16 +34,18 @@ Constraints:
 
 class Solution:
     def orderByOther(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        return [
+            x[0] for x in sorted(zip(nums1, nums2), key=lambda x: x[1])
+        ]
 
 
 sol = Solution()
 
 print(sol.orderByOther([1, 3, 3, 2], [2, 1, 3, 4]))  # [3, 1, 3, 2]
 
-# assert sol.orderByOther([1, 3, 3, 2], [2, 1, 3, 4]) == [3, 1, 3, 2]
-# assert sol.orderByOther([4, 2, 3, 1, 1], [7, 5, 10, 9, 6]) == [2, 1, 4, 1, 3]
-# assert sol.orderByOther([5, 6, 7], [3, 2, 1]) == [7, 6, 5]
-# assert sol.orderByOther([9, 8], [2, 1]) == [8, 9]
-# assert sol.orderByOther([4], [0]) == [4]
-# assert sol.orderByOther(list(range(100000)), list(range(100000))) == list(range(100000))
+assert sol.orderByOther([1, 3, 3, 2], [2, 1, 3, 4]) == [3, 1, 3, 2]
+assert sol.orderByOther([4, 2, 3, 1, 1], [7, 5, 10, 9, 6]) == [2, 1, 4, 1, 3]
+assert sol.orderByOther([5, 6, 7], [3, 2, 1]) == [7, 6, 5]
+assert sol.orderByOther([9, 8], [2, 1]) == [8, 9]
+assert sol.orderByOther([4], [0]) == [4]
+assert sol.orderByOther(list(range(100000)), list(range(100000))) == list(range(100000))
