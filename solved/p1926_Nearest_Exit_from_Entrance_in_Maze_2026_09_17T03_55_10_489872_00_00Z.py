1c55e48a7e749f85d5894f577458e2690a4657ab
shyal <shyal@shyal.com>
2026-09-17 03:55:10 +0000

    1926. Nearest Exit from Entrance in Maze
    solve time: 13m 33s

---
+++ solved/p1926_Nearest_Exit_from_Entrance_in_Maze_2026_09_17T03_55_10_489872_00_00Z.py
@@ -0,0 +1,121 @@
+"""
+URL: https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/description/?envType=problem-list-v2&envId=vn57k9wr
+
+1926. Nearest Exit from Entrance in Maze
+
+You are given an m x n matrix maze (0-indexed) with empty cells (represented as '.') and walls (represented as '+'). You are also given the entrance of the maze, where entrance = [entrance_row, entrance_col] denotes the row and column of the cell you are initially standing at.
+
+In one step, you can move one cell up, down, left, or right. You cannot step into a cell with a wall, and you cannot step outside the maze. Your goal is to find the nearest exit from the entrance. An exit is defined as an empty cell that is at the border of the maze. The entrance does not count as an exit.
+
+Return the number of steps in the shortest path from the entrance to the nearest exit, or -1 if no such path exists.
+
+Example 1:
+
+Input: maze = [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]], entrance = [1,2]
+Output: 1
+Explanation: There are 3 exits in this maze at [1,0], [0,2], and [2,3].
+Initially, you are at the entrance cell [1,2].
+- You can reach [1,0] by moving 2 steps left.
+- You can reach [0,2] by moving 1 step up.
+It is impossible to reach [2,3] from the entrance.
+Thus, the nearest exit is [0,2], which is 1 step away.
+
+Example 2:
+
+Input: maze = [["+","+","+"],[".",".","."],["+","+","+"]], entrance = [1,0]
+Output: 2
+Explanation: There is 1 exit in this maze at [1,2].
+[1,0] does not count as an exit since it is the entrance cell.
+Initially, you are at the entrance cell [1,0].
+- You can reach [1,2] by moving 2 steps right.
+Thus, the nearest exit is [1,2], which is 2 steps away.
+
+Example 3:
+
+Input: maze = [[".","+"]], entrance = [0,0]
+Output: -1
+Explanation: There are no exits in this maze.
+
+Constraints:
+
+    maze.length == m
+    maze[i].length == n
+    1 <= m, n <= 100
+    maze[i][j] is either '.' or '+'.
+    entrance.length == 2
+    0 <= entrance_row < m
+    0 <= entrance_col < n
+    entrance will always be an empty cell.
+
+---
+
+BFS
+
+"""
+
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        q = deque([[entrance[0], entrance[1], 0]])
+        maze[entrance[0]][entrance[1]] = "+"
+
+        while q:
+            x, y, dist = q.popleft()
+            if (
+                (x == 0 or x == len(maze) - 1) or (y == 0 or y == len(maze[0]) - 1)
+            ) and maze[x][y] != "+":
+                return dist
+            maze[x][y] = "+"
+            for nx, ny in nbrs(maze, x, y):
+                if maze[nx][ny] != "+":
+                    q.append([nx, ny, dist + 1])
+
+        return -1
+
+
+sol = Solution()
+
+print(
+    sol.nearestExit(
+        [["+", "+", ".", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]], [1, 2]
+    )
+)  # 1
+
+assert (
+    sol.nearestExit(
+        [["+", "+", ".", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]], [1, 2]
+    )
+    == 1
+)
+assert sol.nearestExit([["+", "+", "+"], [".", ".", "."], ["+", "+", "+"]], [1, 0]) == 2
+assert sol.nearestExit([[".", "+"]], [0, 0]) == -1
+
+assert sol.nearestExit([["."]], [0, 0]) == -1
+# assert sol.nearestExit([["." for _ in range(100)] for _ in range(100)], [50, 50]) == 49
+assert sol.nearestExit([["+"] * 100 for _ in range(100)], [0, 0]) == -1
+assert sol.nearestExit([["."] * 5 for _ in range(1)], [0, 2]) == 1
+assert sol.nearestExit([["."] * 1 for _ in range(5)], [2, 0]) == 1
+assert (
+    sol.nearestExit(
+        [["+", ".", "+", "."], [".", ".", ".", "+"], ["+", "+", "+", "."]], [1, 1]
+    )
+    == 1
+)
+assert sol.nearestExit([[".", ".", "."], [".", ".", "."], [".", ".", "."]], [1, 1]) == 1
+assert sol.nearestExit([["+", ".", "+"], ["+", ".", "+"], ["+", ".", "+"]], [1, 1]) == 1
+assert sol.nearestExit([["."] * 10 for _ in range(10)], [0, 0]) == 1
+assert sol.nearestExit([["."] * 10 for _ in range(10)], [9, 9]) == 1
+assert (
+    sol.nearestExit(
+        [
+            ["+", ".", "+", "+", "+"],
+            ["+", ".", "+", ".", "+"],
+            ["+", ".", ".", ".", "+"],
+            ["+", ".", "+", ".", "+"],
+            ["+", ".", "+", "+", "+"],
+        ],
+        [2, 2],
+    )
+    == 3
+)
+assert sol.nearestExit([["."] * 100], [0, 0]) == 1
